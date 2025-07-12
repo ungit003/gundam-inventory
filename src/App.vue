7fdc21218e664b67adba3572c53585dc7a75489f
ungit003 <ungit003@gmail.com>
2025-07-12 14:56:57 +0900

    건담 재고 관리 프로젝트 시작

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// 앞으로 필요한 코드를 여기에 작성합니다.
+</script>
+
+<template>
+  <!-- 화면에 표시될 모든 내용은 이 안에 작성됩니다. -->
+  <main>
+    <h1>건담 재고 관리 프로젝트 시작!</h1>
+  </main>
+</template>
+
+<style scoped>
+/* 이 컴포넌트에만 적용될 스타일을 여기에 작성합니다. */
+main {
+  padding: 1rem;
+}
+</style>
